5dd22c6c8cee72d9c60c4e5dbdea8b1d536bae12
Tzu Yin <tzyn.wang@gmail.com>
2023-02-12 17:45:54 +0800

    [feat] AppEntryPoint basic content

--- src/component/App/index.tsx
+++ src/component/App/index.tsx
@@ -1,14 +1,27 @@
 import React, { memo } from 'react';
-import SvgComponent from '@Asset/canvas_stand.svg';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import StyleToggle from '@Component/Common/StyleToggle';
+import ThemeName from '@Component/Common/ThemeName';
+import ThemeProvider from '@Component/Layer/ThemeProvider';
+
 import styles from './index.module.css';
 
-function App(): React.ReactElement {
+function AppEntryPoint(): React.ReactElement {
   /* Main */
   return (
-    <div className={styles.hello}>
-      <SvgComponent width={480} height={480} />
-    </div>
+    <ThemeProvider>
+      <div className={styles.hello}>
+        <ThemeName />
+        <StyleToggle />
+        <Stack direction="row" gap={4}>
+          <Chip label="default filled" color="default" variant="filled" />
+          <Chip label="primary filled" color="primary" variant="filled" />
+          <Chip label="secondary filled" color="secondary" variant="filled" />
+        </Stack>
+      </div>
+    </ThemeProvider>
   );
 }
 
-export default memo(App);
+export default memo(AppEntryPoint);
